e9a37cbc40047770ed91f124cba4c7305929c0db
zeikomi552 <premium.tsubu@gmail.com>
2022-10-09 07:00:16 +0900

    履歴の処理を削除

--- PromptMaker/ViewModels/MainWindowVM.cs
+++ PromptMaker/ViewModels/MainWindowVM.cs
@@ -156,31 +156,6 @@ namespace PromptMaker.ViewModels
         }
         #endregion
 
-        #region 履歴[History]プロパティ
-        /// <summary>
-        /// 履歴[History]プロパティ用変数
-        /// </summary>
-        ModelList<ParameterM> _History = new ModelList<ParameterM>();
-        /// <summary>
-        /// 履歴[History]プロパティ
-        /// </summary>
-        public ModelList<ParameterM> History
-        {
-            get
-            {
-                return _History;
-            }
-            set
-            {
-                if (_History == null || !_History.Equals(value))
-                {
-                    _History = value;
-                    NotifyPropertyChanged("History");
-                }
-            }
-        }
-        #endregion
-
         #region ワードクラウド画像パス[ImagePath]プロパティ
         /// <summary>
         /// ワードクラウド画像パス[ImagePath]プロパティ用変数
@@ -600,7 +575,7 @@ namespace PromptMaker.ViewModels
                     if (wnd != null)
                     {
                         var canvas = wnd!.inkCanvas;
-                        SaveCanvas(canvas, Path.Combine(this.SettingConf.Item.CurrentDir, "inputs", "inpainting", "example_mask.png"));
+                        Utilities.SaveCanvas(canvas, Path.Combine(this.SettingConf.Item.CurrentDir, "inputs", "inpainting", "example_mask.png"));
                     }
                 }
 
@@ -632,43 +607,6 @@ namespace PromptMaker.ViewModels
         }
         #endregion
 
-        #region 画像に文字列を埋め込んで移動する処理
-        /// <summary>
-        /// 画像に文字列を埋め込んで移動する処理
-        /// </summary>
-        /// <param name="path">文字列を埋め込む元画像</param>
-        /// <param name="text">埋め込む文字列</param>
-        /// <param name="keyword">文字列を埋め込んだ画像のフォルダ名とファイル名のプレフィクスになります</param>
-        private void SetDetail(string path, string text, string keyword)
-        {
-            string filename = Path.GetFileName(path);
-            string folderPath = System.IO.Path.GetDirectoryName(path)!;
-            folderPath = Path.Combine(folderPath, keyword);
-            PathManager.CreateDirectory(folderPath);
-
-            using (var bmp = new Bitmap(path))
-            using (var fs = new FileStream(Path.Combine(folderPath, keyword + "_" + filename), FileMode.OpenOrCreate, FileAccess.ReadWrite))
-            {
-                int resizeWidth = bmp.Width / 2;
-                int resizeHeight = bmp.Height / 2;
-                using (var resizebmp = new Bitmap(resizeWidth, resizeHeight))
-                {
-                    using (var g = Graphics.FromImage(resizebmp))
-                    {
-
-                        g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
-                        g.DrawImage(bmp, 0, 0, resizeWidth, resizeHeight);
-                        g.DrawString(text, new Font("Arial", 12), System.Drawing.Brushes.Red, new PointF(10.0f, 10.0f));
-                    }
-
-                    fs.SetLength(0);
-
-                    resizebmp.Save(fs, System.Drawing.Imaging.ImageFormat.Png);
-                }
-            }
-        }
-        #endregion
-
         #region 出力先ディレクトリを選択する
         /// <summary>
         /// 出力先ディレクトリを選択する
@@ -690,43 +628,6 @@ namespace PromptMaker.ViewModels
         }
         #endregion
 
-        #region キャンバスの保存処理
-        /// <summary>
-        /// キャンバスの保存処理
-        /// </summary>
-        /// <param name="element"></param>
-        /// <param name="filePath"></param>
-        private static void SaveCanvas(UIElement element, String filePath)
-        {
-            var bounds = VisualTreeHelper.GetDescendantBounds(element);
-            var width = (int)bounds.Width;
-            var height = (int)bounds.Height;
-
-            // 描画先
-            var drawingVisual = new DrawingVisual();
-            using (var ctx = drawingVisual.RenderOpen())
-            {
-                var vb = new VisualBrush(element);
-                ctx.DrawRectangle(System.Windows.Media.Brushes.Black, null, new Rect(new System.Windows.Point(width, height), new System.Windows.Point(width, height)));
-                ((InkCanvas)element).Strokes.Draw(ctx);
-            }
-
-            // ビットマップに変換
-            var rtb = new RenderTargetBitmap(width, height, 96d, 96d, PixelFormats.Pbgra32);
-            rtb.Render(drawingVisual);
-
-            // エンコーダー
-            BitmapEncoder encoder = new JpegBitmapEncoder();
-            encoder.Frames.Add(BitmapFrame.Create(rtb));
-
-            // ファイル保存
-            using (var fs = File.Create(filePath))
-            {
-                encoder.Save(fs);
-            }
-        }
-        #endregion
-
         #region InkCanvasのオブジェクトを削除
         /// <summary>
         /// InkCanvasのオブジェクトを削除
@@ -744,41 +645,6 @@ namespace PromptMaker.ViewModels
                     var canvas = wnd!.inkCanvas;
                     canvas.Strokes.Clear();
                 }
-            }
-            catch (Exception ex)
-            {
-                ShowMessage.ShowErrorOK(ex.Message, "Error");
-            }
-        }
-        #endregion
-
-        #region 選択行の変更
-        /// <summary>
-        /// 選択行の変更
-        /// </summary>
-        public void SelectionChanged()
-        {
-            try
-            {
-                this.ImagePath = this.History.SelectedItem.OutputFilePath;
-
-            }
-            catch (Exception ex)
-            {
-                ShowMessage.ShowErrorOK(ex.Message, "Error");
-            }
-        }
-        #endregion
-
-        #region 履歴のグリッドダブルクリック時に対象の画像ファイルを開く
-        /// <summary>
-        /// 履歴のグリッドダブルクリック時に対象の画像ファイルを開く
-        /// </summary>
-        public void GridDoubleClick()
-        {
-            try
-            {
-                Process.Start("mspaint", this.History.SelectedItem.OutputFilePath); // 指定したフォルダを開く
             }
             catch (Exception ex)
             {
